9d1c0c036f1a24ef0d801fea562703823fbcb1a4
ZZalbs <57441250+ZZalbs@users.noreply.github.com>
2020-09-01 15:51:20 +0900

    1
    올렸다ㅏㅏㅏㅏㅏ

---
+++ Assets/2. Scripts/Enemy.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+    public string enemyName; // 이름
+    public int enemyScore; // 사망시 점수
+    public Sprite[] mode;  // 피격판정
+    public float speed; // 스피드
+    public int hp; // 체력
+
+    SpriteRenderer enemyRender;
+    Rigidbody2D rigid;
+
+    public GameObject bulletEnemyA;
+    public GameObject bulletEnemyB;
+
+    public float maxShotDelay; // 딜레이 제한하는 변수
+    public float curShotDelay; // 현재 딜레이 계산하는 변수
+    public GameObject playerPlane; // 에너미 스크립트의 플레이어 : 프리팹이라 못넣기 때문에 게임매니저에서 지정해줌
+    PlayerControl playerLogic; // 플레이어컨트롤 스크립트 받아옴
+    bool dieCheck = false;
+
+    Vector3 dirVec;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+;       rigid = GetComponent<Rigidbody2D>();
+        enemyRender = GetComponent<SpriteRenderer>();
+        playerLogic = playerPlane.GetComponent<PlayerControl>();
+
+
+
+    }
+
+    void Update()
+    {
+        Fire();
+        Reload();
+    }
+
+    void Fire()// 공격
+    { 
+        if (curShotDelay < maxShotDelay)
+            return;
+
+        dirVec = playerPlane.transform.position - transform.position;
+
+        if (enemyName == "Enemy_small")
+        {
+            BulletShotEnemy(bulletEnemyA,dirVec,0);
+        }
+        else if (enemyName == "Enemy_big")
+        {
+            BulletShotEnemy(bulletEnemyA, dirVec,5);
+            BulletShotEnemy(bulletEnemyA, dirVec,-5);
+        }
+
+        curShotDelay = 0;
+    }
+
+    void BulletShotEnemy(GameObject kind,Vector3 dir,int move)
+    {
+        if (curShotDelay < maxShotDelay)
+            return;
+        Vector2 bulletPos = new Vector2(transform.position.x + move * 0.1f, transform.position.y-0.2f);
+
+        GameObject bullet = Instantiate(kind, bulletPos, transform.rotation);
+        Rigidbody2D bulletRigid = bullet.GetComponent<Rigidbody2D>();
+        bulletRigid.AddForce(dir,ForceMode2D.Impulse);
+    }
+
+
+
+
+    void Reload()
+    {
+        curShotDelay += Time.deltaTime;
+    }
+
+
+
+    void Onhit(int damage)
+    {
+        hp -= damage;
+        enemyRender.sprite = mode[1];
+        Invoke("modechange",0.1f); // 인보크 : 함수 뽑기 전 선딜레이 
+
+        if (hp <= 0&&!dieCheck)         //사망처리
+        {
+            dieCheck = true;
+            playerLogic.score += enemyScore;
+            Destroy(gameObject);
+        }
+    }
+
+
+
+    void modechange()
+    {
+        enemyRender.sprite = mode[0];
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "NoBulletZone")
+        {
+            Destroy(gameObject);
+        }
+        else if(collision.gameObject.tag == "Bullet")
+        {
+            Onhit(collision.gameObject.GetComponent<Bullet>().dmg);
+        }
+    }
+
+
+
+
+
+}
---
+++ Assets/2. Scripts/GameManager.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] enemyObj;
+    public Transform[] spawnPoint;
+
+    public float maxSpawnDelay;
+    public float curSpawnDelay;
+
+    public GameObject player;
+    public int life; // 목숨
+    public Image[] lifeImage;
+    public GameObject gameover;
+
+    Enemy enemyLogic;
+    GameObject enemy;
+    Rigidbody2D rigidEnemy;
+
+    int ranPos;
+    int ranEnemy;
+
+    public Text scoreText; // 점수 텍스트
+
+    public bool isPlayerImmune; // 플레이어 무적체크
+    public SpriteRenderer playerRender; // 플레이어 무적 이펙트
+    Color playerColor; //플레이어 무적 이펙트 2
+    PlayerControl playerCon;
+    PlayerControl playerLogic; // 플레이어컨트롤 스크립트 받아옴
+    
+
+
+
+    Coroutine runningImmune; // 이뮨변수 하나만 만들기
+
+    void Start()
+    {
+        life = 3;
+        playerLogic = player.GetComponent<PlayerControl>();
+        playerRender = player.GetComponent<SpriteRenderer>();
+        playerCon = player.GetComponent<PlayerControl>();
+        playerColor = playerRender.color;
+        isPlayerImmune = false;
+        runningImmune = null;
+
+        for (int index = 0; index < life; index++)                 // 라이프이미지 처리
+            lifeImage[index].color = new Color(1, 1, 1, 1);
+        gameover.SetActive(false);  // 게임오버 이미지 초기화
+    }
+
+    void Update()
+    {
+        curSpawnDelay += Time.deltaTime;
+        if(maxSpawnDelay<curSpawnDelay)
+        {
+            SpawnEnemy();
+            curSpawnDelay = 0;
+            maxSpawnDelay = Random.Range(1.0f,2.0f);
+        }
+        scoreText.text = string.Format("{0:n0}",playerLogic.score); // 세 자리마다 쉼표찍어서 출력하는 포맷
+
+        if(life<=0)
+        {
+            GameOver();
+        }
+    }
+
+    void SpawnEnemy() // 적 소환
+    {
+        ranPos = Random.Range(0, 9);
+        ranEnemy = Random.Range(0, 5);
+        switch (ranEnemy % 3)
+        {
+            case 0:
+                StartCoroutine("Small");
+                break;
+            case 1:
+            case 2:
+                enemy = Instantiate(enemyObj[ranEnemy % 3], spawnPoint[ranPos].position, spawnPoint[ranPos].rotation);
+                enemyLogic = enemy.GetComponent<Enemy>();
+                enemyLogic.playerPlane = player;
+                rigidEnemy = enemy.GetComponent<Rigidbody2D>();
+                EnemyMove(enemy, ranPos);
+                break;
+        }
+    }
+
+    void EnemyMove(GameObject thisEnemy,int pos) // 적 위치조정
+    {
+        if(pos == 5 || pos == 6)
+        {
+            rigidEnemy.velocity = new Vector2(enemyLogic.speed, -1);
+            thisEnemy.transform.Rotate(Vector3.forward*90);
+        }
+        else if (pos == 7 || pos == 8)
+        {
+            rigidEnemy.velocity = new Vector2(enemyLogic.speed * (-1), -1);
+            thisEnemy.transform.Rotate(Vector3.back*90);
+        }
+        else 
+        {
+            rigidEnemy.velocity = Vector2.down * enemyLogic.speed;
+        }
+    }
+
+    void LifeImgCheck()
+    {
+        for(int index=2;index>life-1;index--)                 // 라이프이미지 처리
+            lifeImage[index].color = new Color(1, 1, 1, 0);
+    }
+
+
+    private IEnumerator Small()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            enemy = Instantiate(enemyObj[ranEnemy % 3], spawnPoint[ranPos].position, spawnPoint[ranPos].rotation);
+            enemyLogic = enemy.GetComponent<Enemy>();
+            enemyLogic.playerPlane = player;
+            rigidEnemy = enemy.GetComponent<Rigidbody2D>();
+            EnemyMove(enemy, ranPos);
+            yield return new WaitForSeconds(0.3f);
+        }
+    }
+
+    public void RespawnPlayer()
+    {
+        StartCoroutine("RespawnPlayer2");
+        
+    }
+
+    public IEnumerator RespawnPlayer2()
+    {
+        life--;
+        LifeImgCheck();
+        yield return new WaitForSeconds(2f);
+        player.transform.position = Vector3.down * 3;
+        player.SetActive(true);
+        if (runningImmune == null) {
+            runningImmune = StartCoroutine(ImmuneMode());
+        }
+    }
+
+    private IEnumerator ImmuneMode()
+    {
+        playerCon.isPlayerImmune = true;
+        for (int i = 0; i < 7; i++)
+        {
+            if (i % 2 == 1)
+            {
+                playerColor = new Color(playerColor.r, playerColor.g, playerColor.b, 0.3f);
+                playerRender.color = playerColor;
+            }
+            else
+            {
+                playerColor = new Color(playerColor.r, playerColor.g, playerColor.b, 1f);
+                playerRender.color = playerColor;
+            }
+            yield return new WaitForSeconds(0.3f);
+        }
+        playerCon.isPlayerImmune = false;
+        runningImmune = null;
+    }
+
+
+    public void GameOver()
+    {
+        gameover.SetActive(true);
+       // Time.timeScale = 0.0f;
+    }
+    public void Retry()
+    {
+       // Time.timeScale = 1.0f;
+        SceneManager.LoadScene(0);
+        
+    }
+}
