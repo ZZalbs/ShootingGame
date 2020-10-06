e5ca340bc0cf8c56d6a46f780c90cab3ff4817cd
Zzalbs <kcjlaze@dimigo.hs.kr>
2020-10-07 08:29:34 +0900

    Revert "Merge branch 'master' of https://github.com/ZZalbs/ShootingGame into master"
    This reverts commit 2b8d7bbcab48dfde5888348373d124f660b15be3, reversing
    changes made to 2f2e477a0f3f8dc1921f5d65bc5f77d58c1f35c7.

--- Assets/Script/qqq/BadBullet_Slow.cs
+++ Assets/Script/qqq/BadBullet_Slow.cs
@@ -6,20 +6,22 @@ public class BadBullet_Slow : Bullet
 {
     [HideInInspector]
     public new string type = "bad_bullet_slow";
- 
 
     private void Update()
     {
         CheckDuration();
     }
 
-    Coroutine corSlowPlayer;
+    Coroutine cur;
     public override void AffectObject(GameObject target) // AffectingObject의 함수를 오버라이딩 // 대상에 영향을 주는 코드
     {
         base.AffectObject(target);
         //if(target.tag == "player")
-        //target.GetComponent<CharacterState>().StartCoroutine();
+        cur = StartCoroutine("SlowPlayer");
+        target.GetComponent<CharacterState>().isSlowed = true;
     }
+    IEnumerator SlowPlayer() { yield return 0; } 
+
     void CheckDuration()
     {
 
--- Assets/Script/qqq/CharacterState.cs
+++ Assets/Script/qqq/CharacterState.cs
@@ -7,40 +7,4 @@ public class CharacterState : MonoBehaviour
     //적과 플레이어의 동일한 상태
     public bool isStunned;
     public bool isSlowed;
-
-    public List<float> SlowAmountLIst;
-    public float SlowAmount;
-
-    PlayerControl pc;
-    CharacterStat cs;
-    private void Start()
-    {
-        pc = GetComponent<PlayerControl>();
-        cs = GetComponent<CharacterStat>();
-        StartCoroutine(CheckMaxAmount());
-    }
-
-    //효과
-    IEnumerator AffectSlow(float amount, float duration)
-    {
-        float slowCurCool = duration;
-        SlowAmountLIst.Add(amount);
-        while(slowCurCool >= 0)
-        {
-            slowCurCool -= Time.deltaTime;
-            yield return null;
-        }
-        SlowAmountLIst.Remove(amount);
-    }
-
-    IEnumerator CheckMaxAmount()
-    {
-        foreach(float amount in SlowAmountLIst)
-        {
-            if (SlowAmount < amount) SlowAmount = amount;
-            yield return null;
-        }
-        pc.speed = cs.MoveSpeed * (SlowAmount / 100);
-        //...
-    }
 }
