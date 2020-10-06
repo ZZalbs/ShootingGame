c74e7a074c9ebba9779d188cde7ccfdc8c7dafd3
jjjwwer <70564930+jjjwwer@users.noreply.github.com>
2020-10-06 16:41:45 +0900

    슬로우효과

---
+++ Assets/Script/qqq/CharacterStat.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStat : MonoBehaviour
+{
+    public float MoveSpeed;
+    public float BulletPower;
+    public float HealPoint;
+    public float FireDelay;
+}
