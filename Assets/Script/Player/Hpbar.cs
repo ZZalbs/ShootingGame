15e7c536bc9889580d98b6935a3e65440c746bba
Zzalbs <kcjlaze@dimigo.hs.kr>
2020-10-13 16:21:23 +0900

    tnwjd

--- Assets/Script/Player/Hpbar.cs
+++ Assets/Script/Player/Hpbar.cs
@@ -2,17 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hpbar : MonoBehaviour
+public class Hpbar : PlayerControl
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+            
     }
 }
