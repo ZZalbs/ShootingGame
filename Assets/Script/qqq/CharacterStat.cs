a437609b15852ae04d87b7125affaeaf429db297
jjjwwer <70564930+jjjwwer@users.noreply.github.com>
2020-10-13 15:12:32 +0900

    충돌

---
+++ Assets/Script/qqq/CharacterStat.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStat : MonoBehaviour
+{
+    public float MoveSpeed;
+    public float FirePower;
+    public float HealPoint;
+}
